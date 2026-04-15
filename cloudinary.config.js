ebb541cbd1e054619ec7f656e0190604dea15262
yunhn03-crypto <yunhn03@gmail.com>
2026-04-15 21:51:36 +0900

    Add files via upload

---
+++ cloudinary.config.js
@@ -0,0 +1,7 @@
+window.CLOUDINARY_CONFIG = {
+  cloudName: "YOUR_CLOUD_NAME",
+  uploadPreset: "YOUR_UNSIGNED_PRESET",
+  folder: "tilted-hannam",
+  sources: ["local", "camera", "url"],
+  multiple: false
+};
